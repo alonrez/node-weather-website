a242bed59e0b7189f1dfef245342810b8c854d60
alonrez <54244917+alonrez@users.noreply.github.com>
2020-10-26 00:17:58 +0200

    V_2.3.0.21

--- public/js/Dark_algo.js
+++ public/js/Dark_algo.js
@@ -18,27 +18,27 @@
 //        }
 // })
  
-function DarkAlgo(x) {
-    document.body.style.backgroundColor = '#00bfff';
-    document.getElementById('main-content').style.color = '#3d3a3a';
-    document.getElementById('button').innerText = 'Dark-Mode';
-}
-function LightAlgo(x) {
-    document.body.style.backgroundColor = '#1f1f1f';
-    document.getElementById('main-content').style.color = '#7fffd4';
-    document.getElementById('button').innerText = 'Light-Mode';
-}
+// function DarkAlgo(x) {
+//     document.body.style.backgroundColor = '#00bfff';
+//     document.getElementById('main-content').style.color = '#3d3a3a';
+//     document.getElementById('button').innerText = 'Dark-Mode';
+// }
+// function LightAlgo(x) {
+//     document.body.style.backgroundColor = '#1f1f1f';
+//     document.getElementById('main-content').style.color = '#7fffd4';
+//     document.getElementById('button').innerText = 'Light-Mode';
+// }
 
 
 
-document.getElementById('button').addEventListener("click", function () {
-    if ( x === undefined) {
-        x = 1;
-    } else if (x % 2 == 0) {
-      DarkAlgo(x)
-      x++;
-    } else {
-        LightAlgo(x)
-        x++;
-    }
-})
+// document.getElementById('button').addEventListener("click", function () {
+//     if ( x === undefined) {
+//         x = 1;
+//     } else if (x % 2 == 0) {
+//       DarkAlgo(x)
+//       x++;
+//     } else {
+//         LightAlgo(x)
+//         x++;
+//     }
+// })
